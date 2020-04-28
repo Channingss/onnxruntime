83896bdb5e33c05249ce27cc1da5808fa1793a17
Changming Sun <chasun@microsoft.com>
2020-04-27 19:38:43 -0700

    a

--- onnxruntime/test/framework/op_kernel_test.cc
+++ onnxruntime/test/framework/op_kernel_test.cc
@@ -2,13 +2,11 @@
 // Licensed under the MIT License.
 #include "core/graph/onnx_protobuf.h"
 
-#include "core/session/inference_session.h"
-#include "core/framework/op_kernel.h"
-#include "core/framework/session_state.h"
 #include "core/graph/graph_viewer.h"
 #include "core/graph/model.h"
 #include "core/graph/op.h"
-#include "core/providers/cpu/cpu_execution_provider.h"
+#include "core/framework/op_kernel.h"
+#include "core/framework/session_state.h"
 #include "gtest/gtest.h"
 #include "test_utils.h"
 using namespace std;
@@ -18,11 +16,6 @@ namespace onnxruntime {
 using namespace logging;
 
 namespace test {
-
-class XPUExecutionProvider : public IExecutionProvider {
- public:
-  XPUExecutionProvider() : IExecutionProvider{onnxruntime::kCpuExecutionProvider} {}
-};
 
 }  // namespace test
 }  // namespace onnxruntime
